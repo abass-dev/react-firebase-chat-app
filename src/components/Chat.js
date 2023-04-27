66d046d0d9005ec77be09dfaea482f43879eb4f0
abass-dev <abassthedev@gmail.com>
2023-04-27 22:49:34 +0100

    pause first app

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,50 +1,96 @@
-import { useState, useEffect , useRef } from 'react'
-import { addDoc, collection, serverTimestamp, onSnapshot, query, where, orderBy} from 'firebase/firestore'
-import { auth, db } from '../firebase-config'
+import {
+  useState,
+  useEffect,
+  useRef
+} from 'react'
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+  orderBy
+} from 'firebase/firestore'
+import {
+  auth,
+  db
+} from '../firebase-config'
 import '../styles.css'
 
 export const Chat = (props) => {
-  
-  const {room} = props
-  const [newMessage, setNewMessage] = useState('')
-  const [messages, setMessages] = useState([])
+
+  const {
+    room
+  } = props
+  const [newMessage,
+    setNewMessage] = useState('')
+  const [messages,
+    setMessages] = useState([])
   const messageRef = collection(db, 'messages')
-  
+
   useEffect(() => {
     const queryMessages = query(messageRef, where('room', '==', room), orderBy('createdAt'))
     let msgs = []
     const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
       const newData = snapshot.docs.map(doc => {
-       return {id: doc.id, ...doc.data()}
+        return {
+          id: doc.id, ...doc.data()}
       });
-      setMessages(newData); 
-    //console.log(messages);
+      setMessages(newData);
+      //console.log(messages);
     })
     return () => unsubscribe()
   }, [])
   const onSubmitHandler = async (e) => {
     e.preventDefault()
-    if(newMessage === '') return
-    
+    if (newMessage === '') return
+
     await addDoc(messageRef, {
       text: newMessage,
       user: auth.currentUser.displayName,
       image: auth.currentUser.photoURL,
       createdAt: serverTimestamp(),
+      time: new Date().toString(),
       room
     })
     setNewMessage('')
   }
+  
+  const handleTime = (time) => {
+    // Create a new Date object
+    const now = new Date(time);
+
+    // Get the current hour and minutes
+    const hour = now.getHours();
+    const minutes = now.getMinutes();
+    // Determine whether it's AM or PM
+    const amOrPm = hour >= 12 ? 'PM': 'AM';
+
+    // Convert the hour to 12-hour format
+    const hour12 = hour % 12 || 12;
+
+    // Output the current time in "hh:mm AM/PM" format
+    return `${hour12}:${minutes <= 9 ? '0' : ''}${minutes} ${amOrPm}`;
+
+  }
   return (
-    <div className='chat-form-control' >
+    <div className='chat-form-control'>
     {messages.map((message) => {
-       return (
-       <div className='chat-interface' key={message.id}>
+      return (
+        <div className='chat-interface' key={message.id}>
        <img className='chat-user-icon' src={message.image} />
-       <p className='chat-username'>{message.user}</p>
-       <p className='chat-message'>{message.text}</p>
-       </div>
-       )
+       <p className='chat-username'>
+          {message.user}
+        </p>
+       <p className='chat-message'>
+          {message.text}
+        </p>
+       <p className='chat-time'>
+          {handleTime(message.time)}
+        </p>
+        </div>
+      )
     })}
     <form className='chat-form' onSubmit={onSubmitHandler}>
     <div className='chat-control'>
@@ -53,5 +99,5 @@ export const Chat = (props) => {
     </div>
     </form>
     </div>
-    )
+  )
 }
